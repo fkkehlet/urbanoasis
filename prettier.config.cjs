11b3f77f7c4e670e89e14d7d9063c2e21b814550
fkkehlet <developmedaddy@aliensz.org>
2023-07-13 00:03:27 +0200

    Start work on site

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,15 +1,22 @@
 module.exports = {
-	plugins: [
-		require.resolve('prettier-plugin-astro'),
-		require('prettier-plugin-tailwindcss'),
-	],
-	overrides: [
-		{
-			files: '*.astro',
-			options: {
-				parser: 'astro',
-			},
-		},
-	],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require('prettier-plugin-tailwindcss'),
+  ],
+  htmlWhitespaceSensitivity: 'ignore',
+  printWidth: 120,
+  proseWrap: 'never',
+  // semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
-
