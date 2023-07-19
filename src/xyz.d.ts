90cc8d0672040f7964242b758a05fb9e45436158
fkkehlet <developmedaddy@aliensz.org>
2023-07-19 07:21:52 +0200

    Play around with animation API

--- src/xyz.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="astro/astro-jsx" />
-
-type MyAttributes = astroHTML.JSX.ImgHTMLAttributes;
-
-declare namespace astroHTML.JSX {
-  interface HTMLAttributes {
-    xyz?: number;
-  }
-}
